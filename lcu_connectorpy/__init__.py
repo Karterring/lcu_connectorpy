b186bf7cc645aa47add9e9af7e9a3b9bdf593e5c
Noah <noah@coronasoftware.net>
2019-04-15 15:43:11 -0700

    Fixed tracking issue

--- lcu_connectorpy/__init__.py
+++
@@ -1,5 +0,0 @@
-from .connect import Connector
-
-__all__ = ['Connector']
-
-__version__ = '0.1.0'
--- scripts.py
+++
@@ -1,8 +0,0 @@
-import subprocess as subp
-
-
-def doc():
-    subp.run([
-        'pdoc', '--html', '--overwrite',
-        '--html-dir', 'doc', 'lcu_connectorpy'
-    ])
